b4287580a58ed01b7d966fc82b060e712f0d7d76
Frank000000 <2469739226@qq.com>
2022-09-28 14:14:23 +0800

    chore:add umd import

--- src/export.ts
+++ src/export.ts
@@ -1,3 +1,12 @@
 import TestComponent from '@/components/TestComponent.vue';
 import testStyle from '@/style.css'
-export { TestComponent,testStyle }
+const components = [TestComponent];
+// 注册组件
+const install = function (App: any, options: any) {
+  components.forEach((component) => {
+    App.component(component.name, component);
+  });
+};
+export { install, testStyle, TestComponent }
+
+// export {testStyle,TestComponent}
